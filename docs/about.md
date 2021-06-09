864971f10255d3bea85a3aa2317caa5adcbc2fa6
Naofumi Yamada <17900178+na0fu3y@users.noreply.github.com>
2021-06-09 18:33:52 +0900

    docusaurus init

---
+++ docs/about.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# BQ FUNとは
+
+BigQuery依存症の方を増やすための組織です。
+
+## GitHub
+
+https://github.com/bqfun
+
+## Slack
+
+https://bqfun.slack.com
+
+新規参加は[こちら](https://join.slack.com/t/bqfun/shared_invite/zt-p14l1byc-rIZ2WmndLdzFBT63HRVgfA)
---
+++ docusaurus.config.js
@@ -0,0 +1,44 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'BQ FUN',
+  tagline: 'BigQuery中毒者へ',
+  url: 'https://bqfun.jp',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'bqfun', // Usually your GitHub org/user name.
+  projectName: 'bqfun', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'BQ FUN',
+      logo: {
+        alt: 'BQ FUN Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/bqfun/bqfun',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href: 'https://bqfun.slack.com',
+          label: 'Slack',
+          position: 'right',
+        },
+      ],
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/bqfun/bqfun/edit/main/',
+          routeBasePath: '/',
+        },
+      },
+    ],
+  ],
+};
