ab7347cc58a93469560c3caf99eb944a6332f259
yamadanaofumi <17900178+na0fu3y@users.noreply.github.com>
2022-07-31 21:42:01 +0900

    docusaurus rc1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,61 +1,136 @@
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'BQ FUN',
-  favicon: 'favicon.ico',
+  tagline: 'BigQuery中毒者へ',
   url: 'https://bqfun.jp',
   baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'bqfun', // Usually your GitHub org/user name.
+  projectName: 'bqfun', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja'],
   },
-  tagline: 'BigQuery中毒者へ',
-  organizationName: 'bqfun', // Usually your GitHub org/user name.
-  projectName: 'bqfun', // Usually your repo name.
-  themeConfig: {
-    hideableSidebar: true,
-    image: 'img/bqfun.png',
-    navbar: {
-      title: 'BQ FUN',
-      logo: {
-        alt: 'BQ FUN Logo',
-        src: 'img/bqfun.png',
-      },
-      items: [
-        {
-          href: 'https://github.com/bqfun/bqfun',
-          label: 'GitHub',
-          position: 'right',
-        },
-        {
-          href: 'https://bqfun.slack.com',
-          label: 'Slack',
-          position: 'right',
-        },
-        {
-          href: 'https://trello.com/b/EHyZuRFS/bq-fun-issues',
-          label: 'Trello',
-          position: 'right',
-        },
-      ],
-    },
-    googleAnalytics: {
-      trackingID: 'UA-156581645-4',
-    },
-    gtag: {
-      trackingID: 'G-62K9N310Q2',
-    },
-  },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bqfun/bqfun/edit/main/',
-          routeBasePath: '/',
+            'https://github.com/bqfun/bqfun/',
         },
-      },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/bqfun/bqfun/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-62K9N310Q2',
+          anonymizeIP: true,
+        },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'BQ FUN',
+        logo: {
+          alt: 'BQ FUN Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduction',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/bqfun',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'BQ FUNとは',
+                to: '/docs/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Slack',
+                href: 'https://bqfun.slack.com',
+              },
+              {
+                label: 'Trello',
+                href: 'https://trello.com/b/EHyZuRFS/bq-fun-issues',
+              },
+              {
+                label: 'connpass',
+                href: 'https://bqfun.connpass.com/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/bqfun',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} BQ FUN. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
+
+module.exports = config;
